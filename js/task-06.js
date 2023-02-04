131164fed3b18f7d196390bd2eb89f80f82607ae
Алексей <electrifyx@gmail.com>
2023-02-04 11:22:45 +0200

    edit to homework

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,11 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener ('blur', onInputBlur => {
+inputEl.addEventListener ('blur', event => {
     inputEl.classList.remove("valid", "invalid");
 
     const inputColorEl = (inputEl.value.length === Number(inputEl.dataset.length)/*6*/)
            ? inputEl.classList.add("valid")
         : inputEl.classList.add("invalid");
-    return inputEl;
-});
+    });
 
 
--- js/task-08.js
+++ js/task-08.js
@@ -1,74 +1,22 @@
 const form = document.querySelector('.login-form');
 const inputs = document.querySelectorAll('input');
 
-form.addEventListener('submit', onFormSubmit);
-
-function onFormSubmit(event) {
+form.addEventListener('submit', event => {
     event.preventDefault();
 
-    inputs.forEach(input => {
-        if (input.value.trim() === '') {
-            alert("Заполните все поля!")
-        }
-        
-        const formElements = event.currentTarget.elements;
-        
-        const mail = formElements.email.value;
-        const password = formElements.password.value;
-        
-        console.log(mail, password);
+    if (form.email.value === '' || form.password.value === '') {
+        return alert('Заполните все поля!');
+    }
+
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    // console.log(mail, password);
+    console.log(`Login: ${form.email.value}, Password: ${form.password.value}`);
+    form.reset();
 });
-   
-//     const formData = new FormData(event.currentTarget);
-//     console.log(formData);
-
-//     formData.forEach((value, name) => {
-//         console.log('onFormSubmit -> name', name);
-//         console.log('onFormSubmit -> value', value);
-// })
-
-form.reset();
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
